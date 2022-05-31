85f4f7dac65cd49a9f85c01b1b2a570fd0e1c359
Truong <dotientruong.hn1@gmail.com>
2022-05-31 23:28:51 +0700

    comment

---
+++ test.js
@@ -0,0 +1,30 @@
+//bai 1
+// const a = Number(prompt(`nhap so a`));
+// const b = Number(prompt(`nhap so b`));
+
+// for (let i = a; i < b; i++) {
+//     if 
+// }
+
+
+// bai 2
+// let oneRow = "";
+// function numberOneTriangle(number)
+// {
+//         for(let i=0;i<=number;i++)
+//     {
+// 	    oneRow += "*"
+//         console.log(oneRow)
+// 	}
+// }
+
+// numberOneTriangle(4)
+
+
+
+// let a = parseInt(prompt('nhap so a: '))
+// let b = parseInt(prompt('nhap so b: '))
+// let array = [a, b];
+
+
+// console.log(array[2]);
